6ef6c5170343a2f69d4438fd5b087e5207f3591e
Andrea Contina <acontina@outlook.com>
2024-11-11 11:02:42 -0600

    second version of DDA.R

--- DDA.R
+++ DDA.R
@@ -1,4 +1,4 @@
-   # Simple R script to demonstrate version control
-   data <- c(1, 2, 3, 4, 5)
+   # Simple R script to demonstrate version control more DATA
+   data <- c(1, 2, 3, 4, 5, 6, 76, 564, 234, 432, 323, 6, 8, 9, 212)
    mean_data <- mean(data)
    print(paste("The mean of the data is:", mean_data))
